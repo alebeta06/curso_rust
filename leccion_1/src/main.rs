bd9f105f88a2e12d5f3b8e14ab2d1198a8112663
alebeta <alebeta0000@gmail.com>
2025-04-01 02:35:26 -0500

    los 2 primeros ejercicios

--- leccion_1/src/main.rs
+++ leccion_1/src/main.rs
@@ -1,3 +1,14 @@
+//Ejercicio 1: Hola Mundo
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rust!");
+
+    //Ejercicio 2: Variables y Mutabilidad
+    let x = 10;
+    // x = 20; // Esto causará un error porque x es inmutable
+
+    let mut y = 5;
+    y = 15; // Esto es válido porque y es mutable
+
+    const PI: f64 = 3.1416;
+    println!("x: {}, y: {}, PI: {}", x, y, PI);
 }
