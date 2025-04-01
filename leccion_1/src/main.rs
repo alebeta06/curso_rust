3ad295cc5a2d21ec175fca04050da3ac13eaad52
alebeta <alebeta0000@gmail.com>
2025-04-01 02:47:35 -0500

    Ejercicio 4

--- leccion_1/src/main.rs
+++ leccion_1/src/main.rs
@@ -15,4 +15,9 @@ fn main() {
     // Ejercicio 3: Tipos de Datos y Tuplas
     let datos: (i32, f64, char) = (42, 3.14, 'R');
     println!("Entero: {}, Flotante: {}, Carácter: {}", datos.0, datos.1, datos.2);
+
+    // Ejercicio 4: Conversión de Tipos (Casting)
+    let numero: i32 = 25;
+    let numero_flotante: f64 = numero as f64;
+    println!("Número entero: {}, Número flotante: {}", numero, numero_flotante);
 }
