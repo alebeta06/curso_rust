388f960dbfd3684a4e112260d8c64e98500872c8
alebeta <alebeta0000@gmail.com>
2025-04-06 21:20:25 -0500

    Implement exercise to display the day of the week based on user input

--- leccion_2/src/main.rs
+++ leccion_2/src/main.rs
@@ -1,3 +1,28 @@
+use std::io; // Para leer entrada del usuario
+
+// -------------------------
+// EJERCICIO 1
+// Programa que acepta un número del 1 al 7 y muestra el día de la semana
+// -------------------------
+fn mostrar_dia(numero: u8) {
+    match numero {
+        1 => println!("Lunes"),
+        2 => println!("Martes"),
+        3 => println!("Miércoles"),
+        4 => println!("Jueves"),
+        5 => println!("Viernes"),
+        6 => println!("Sábado"),
+        7 => println!("Domingo"),
+        _ => println!("Número inválido. Debe ser del 1 al 7."),
+    }
+}
+
+// -------------------------
+// FUNCIÓN PRINCIPAL
+// -------------------------
 fn main() {
-    println!("Hello, world!");
+    // -------------------------
+    // Ejecutamos Ejercicio 1
+    println!("--- EJERCICIO 1 ---");
+    mostrar_dia(3); // Puedes cambiar el número del 1 al 7
 }
---
+++ leccion_2/target/debug/leccion_2.d
@@ -0,0 +1,1 @@
+/home/alebeta/curso_rust/leccion_2/target/debug/leccion_2: /home/alebeta/curso_rust/leccion_2/src/main.rs
