23a4c6afadd5310754ddcfcb8a19e2c63ef08cba
alebeta <alebeta0000@gmail.com>
2025-04-15 02:29:36 -0500

    add

--- leccion_3/src/main.rs
+++ leccion_3/src/main.rs
@@ -1,10 +1,15 @@
-const X: i32 = 10;
-
 fn main() {
-    println!("{}", X);
-    child();
+   let x: &str = "Hola";
+   let y: &str = "Mundo";
+
+   let z: &str = larga(x, y);
+   println!("La cadena más larga es: {}", z);
 }
 
-fn child() {
-    println!("{}", X);
-}
+fn larga<'amigo>(x: &'amigo str, y: &'amigo str) -> &'amigo str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+} 
